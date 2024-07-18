5ec7bfba26e916cf333e6add8b15a33850b09cec
Your Name <dpacoria@unsa.edu.pe>
2024-07-18 14:06:54 -0500

    Se agrego padding a la pagina de inicio

--- src/app/(ecologic)/layout.tsx
+++ src/app/(ecologic)/layout.tsx
@@ -4,7 +4,7 @@ import { TopMenu } from '@/components/top-menu/TopMenu';
 
 export default function EcologicLayout ({children}: Readonly<{children: React.ReactNode}>) {
     return (
-        <main className='min-h-screen min-w-screen bg-gradient-to-br from-green-400 to-green-200'>
+        <main className='min-h-screen min-w-screen bg-gradient-to-br from-green-400 to-green-200 p-5'>
 
             <TopMenu />
             <Sidebar />
